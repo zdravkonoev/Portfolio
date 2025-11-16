7293483a45d2ad360a0f2f601a6bfc568520a5bd
Zdravko Noev <znoev@hotmail.com>
2025-11-16 04:02:49 +0200

    Tailwind CSS installed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
-import App from './App.jsx'
+import App from './App.jsx';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
