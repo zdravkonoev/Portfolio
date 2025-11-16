2e6be7b683549a57579c6e1297c72df674116072
Zdravko Noev <znoev@hotmail.com>
2025-11-16 02:27:59 +0200

    Add React Router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,9 @@
-import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <App />
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 )
