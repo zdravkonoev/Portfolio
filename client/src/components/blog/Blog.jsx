a9c04c104fb9fac27ad9d5604672d7bc2dfd6142
Zdravko Noev <znoev@hotmail.com>
2025-11-23 21:33:59 +0200

    Hide Header if I'm not on Blog page

--- client/src/components/blog/Blog.jsx
+++ client/src/components/blog/Blog.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react'
 import { useState } from 'react';
 import Post from './Post.jsx';
+import Header from '../header/Header.jsx'
+import { useLocation } from 'react-router-dom';
 
 export default function Blog() {
   const [posts, setPosts] = useState([]);
@@ -20,30 +22,35 @@ export default function Blog() {
       }); 
   }, []);
 
+  // To remove Header if I'm not in Blog Page
+  const { pathname } = useLocation();
 
   return (
-    <div className="bg-white py-24 sm:py-32">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto max-w-2xl lg:mx-0">
-          <h2 className="text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl">Blog</h2>
-          <p className="mt-2 text-lg/8 text-gray-600">Here are the post form my colleagues about my work wiht them.</p>
-        </div>
-        <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-          {posts.map((post) => (
-              <Post 
-                key={post.id}
-                _id={post.id} 
-                title={post.title} 
-                date={post.date}
-                datetime={post.datetime} 
-                category= {post.category}
-                description={post.description} 
-                author={post.author} 
-                href={post.href} 
-              />
-            ))}
+    <>
+      {pathname === "/blog" && <Header />}
+      <div className="bg-gray-900 py-24 sm:py-32">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl lg:mx-0">
+            <h2 className="text-4xl font-semibold tracking-tight text-pretty text-white sm:text-5xl">Blog</h2>
+            <p className="mt-2 text-lg/8 text-gray-300">Here are the post form my colleagues about my work wiht them.</p>
+          </div>
+          <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-700 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+            {posts.map((post) => (
+                <Post 
+                  key={post.id}
+                  _id={post.id} 
+                  title={post.title} 
+                  date={post.date}
+                  datetime={post.datetime} 
+                  category= {post.category}
+                  description={post.description} 
+                  author={post.author} 
+                  href={post.href} 
+                />
+              ))}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
--- client/src/components/blog/Post.jsx
+++ client/src/components/blog/Post.jsx
@@ -10,39 +10,39 @@ export default function Post({
 }) {
 
 return (
-        <article key={id} className="flex max-w-xl flex-col items-start justify-between">
-            <div className="flex items-center gap-x-4 text-xs">
-            <time dateTime={datetime} className="text-gray-500">
-                {date}
+    <article key={id} className="flex max-w-xl flex-col items-start justify-between">
+        <div className="flex items-center gap-x-4 text-xs">
+            <time dateTime={datetime} className="text-gray-400">
+            {date}
             </time>
             <a
-                href={category.href}
-                className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
+            href={category.href}
+            className="relative z-10 rounded-full bg-gray-800/60 px-3 py-1.5 font-medium text-gray-300 hover:bg-gray-800"
             >
-                {category.title}
+            {category.title}
             </a>
-            </div>
-            <div className="group relative grow">
-            <h3 className="mt-3 text-lg/6 font-semibold text-gray-900 group-hover:text-gray-600">
-                <a href={href}>
+        </div>
+        <div className="group relative grow">
+            <h3 className="mt-3 text-lg/6 font-semibold text-white group-hover:text-gray-300">
+            <a href={href}>
                 <span className="absolute inset-0" />
                 {title}
-                </a>
+            </a>
             </h3>
-            <p className="mt-5 line-clamp-3 text-sm/6 text-gray-600">{description}</p>
-            </div>
-            <div className="relative mt-8 flex items-center gap-x-4 justify-self-end">
-            <img alt="" src={author.imageUrl} className="size-10 rounded-full bg-gray-50" />
+            <p className="mt-5 line-clamp-3 text-sm/6 text-gray-400">{description}</p>
+        </div>
+        <div className="relative mt-8 flex items-center gap-x-4 justify-self-end">
+            <img alt="" src={author.imageUrl} className="size-10 rounded-full bg-gray-800" />
             <div className="text-sm/6">
-                <p className="font-semibold text-gray-900">
+            <p className="font-semibold text-white">
                 <a href={author.href}>
-                    <span className="absolute inset-0" />
-                    {author.name}
+                <span className="absolute inset-0" />
+                {author.name}
                 </a>
-                </p>
-                <p className="text-gray-600">{author.role}</p>
+            </p>
+            <p className="text-gray-400">{author.role}</p>
             </div>
-            </div>
-        </article>
+        </div>
+    </article>
     )
 }
